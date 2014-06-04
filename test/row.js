a130aece4a5a03fba7f2194ea50237f7fc68b2c7
Mikołaj Banasik <d1sover@gmail.com>
2014-06-05 00:35:30 +0100

    Finished implementation and tests for Row object.

--- test/row.js
+++ test/row.js
@@ -64,7 +64,7 @@
 
         });
         
-        xdescribe("modification", function() {
+        describe("modification", function() {
             
             var columns,
                 fields;
@@ -74,19 +74,62 @@
                 fields = ["one", "two", "three", "four"];
             });
             
-            it("does not allow to modify any of the index properties");
+            it("does not allow for modification any of the index properties", function() {
+
+                var testRow = new Row(columns, fields);
+
+                expect(function() { testRow[0] = "notOne";   }).to.throw(TypeError, /Cannot assign to read only property/);
+                expect(function() { testRow[1] = "notTwo";   }).to.throw(TypeError, /Cannot assign to read only property/);
+                expect(function() { testRow[2] = "notOhree"; }).to.throw(TypeError, /Cannot assign to read only property/);
+                expect(function() { testRow[3] = "notFour";  }).to.throw(TypeError, /Cannot assign to read only property/);
+                
+                expect(function() { delete testRow[0]; }).to.throw(TypeError, /Cannot delete property/);
+                expect(function() { delete testRow[1]; }).to.throw(TypeError, /Cannot delete property/);
+                expect(function() { delete testRow[2]; }).to.throw(TypeError, /Cannot delete property/);
+                expect(function() { delete testRow[3]; }).to.throw(TypeError, /Cannot delete property/);
+            });
             
-            it("does not allow to modify any of the column name properties");
+            it("does not allow for addition of new index properties", function() {
+
+                var testRow = new Row(columns, fields);
+                
+                expect(function() { testRow[-1] = "zero"; }).to.throw(TypeError, /Can\'t add property/);
+                expect(function() { testRow[4] = "five";  }).to.throw(TypeError, /Can\'t add property/);
+            });
             
-            it("does not allow to modify length");
+            it("does not allow for modification of any of the column name properties", function() {
+                
+                var testRow = new Row(columns, fields);
+
+                expect(function() { testRow.spam = "notOne";    }).to.throw(TypeError, /Cannot assign to read only property/);
+                expect(function() { testRow.ham = "notTwo";     }).to.throw(TypeError, /Cannot assign to read only property/);
+                expect(function() { testRow.eggs = "notOhree";  }).to.throw(TypeError, /Cannot assign to read only property/);
+                expect(function() { testRow.bacon = "notFour";  }).to.throw(TypeError, /Cannot assign to read only property/);
+                
+                expect(function() { delete testRow.spam;  }).to.throw(TypeError, /Cannot delete property/);
+                expect(function() { delete testRow.ham;   }).to.throw(TypeError, /Cannot delete property/);
+                expect(function() { delete testRow.eggs;  }).to.throw(TypeError, /Cannot delete property/);
+                expect(function() { delete testRow.bacon; }).to.throw(TypeError, /Cannot delete property/);
+            });
+            
+            it("does not allow for addition of new named properties", function() {
+                
+                var testRow = new Row(columns, fields);
+                
+                expect(function() { testRow.test = "zero";     }).to.throw(TypeError, /Can\'t add property/);
+                expect(function() { testRow.toString = "five"; }).to.throw(TypeError, /Can\'t add property/);
+            });
+            
+            it("does not allow for change to length", function() {
+                
+                var testRow = new Row(columns, fields);
+                
+                expect(function() { testRow.length = 2; }).to.throw(TypeError, /Cannot assign to read only property/);
+            });
             
         });
         
         xdescribe("generation from result", function() {
-            
-        });
-        
-        xdescribe("array methods", function() {
             
         });
         
